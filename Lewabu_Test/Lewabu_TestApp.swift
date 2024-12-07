dd1a7a3a92ff980f2ad08324e978d4017b0b931a
lue-commits <Laurenzbuisness@gmail.com>
2024-12-07 14:26:23 +0100

    Initial Commit

---
+++ Lewabu_Test/Lewabu_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lewabu_TestApp.swift
+//  Lewabu_Test
+//
+//  Created by Laurenz Kersting on 07.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Lewabu_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lewabu_TestTests/Lewabu_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lewabu_TestTests.swift
+//  Lewabu_TestTests
+//
+//  Created by Laurenz Kersting on 07.12.24.
+//
+
+import Testing
+@testable import Lewabu_Test
+
+struct Lewabu_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
